49bdbfc873885d81ee69c68c14bd350e72c7ba4b
Peter Alexander 捷虎 Graham <peter_graham@brown.edu>
2024-08-13 15:27:12 -0400

    Create Visualization.jl

---
+++ src/Visualization.jl
@@ -0,0 +1,13 @@
+module Visualization
+
+using Plots
+using LinearAlgebra
+
+"""
+Plot the density matrix of a quantum state.
+"""
+function plot_density_matrix(density_matrix::AbstractMatrix)
+    heatmap(abs2.(density_matrix), c=:viridis, title="Density Matrix", xlabel="Basis State", ylabel="Basis State")
+end
+
+end
